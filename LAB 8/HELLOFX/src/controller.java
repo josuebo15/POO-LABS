8f076addbb884dc291492156d29519d6d8172124
josuebo15 <62129593+josuebo15@users.noreply.github.com>
2020-07-13 23:57:09 -0600

    Add files via upload

---
+++ LAB 8/HELLOFX/src/controller.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+/**
+ *
+ * @author estudiante
+ */
+public class controller {
+    
+    @FXML
+    private Label label;
+
+    public void initialize() {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+    }
+}
---
+++ LAB 8/HELLOFX/src/hellofx/HELLOFX.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellofx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class HELLOFX extends Application{
+    
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getclass().getResource("hellofx.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new scene(root));
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch (args)
+        // TODO code application logic here
+    }
+    
+}
